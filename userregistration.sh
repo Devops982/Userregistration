247e36f934d8634d8566f7172501ef8edb43e7fe
“Enter <talktoparthi@gmail.com>
2020-08-07 19:35:24 +0530

    User enters firstname - minimum 3letters first letter capital

--- userregistration.sh
+++ userregistration.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 
+
+echo "Enter your first name :" 
+
+read firstName 
+
+pattern="^[A-Z]{1}[a-z]{2}$" 
+
+if [[ $firstName =~ $pattern ]] 
+
+then echo "Name is valid"
+
+ else echo "Name is invalid"
+
+fi
