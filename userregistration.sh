a0d9946c8a127a7dcb1c4c1692e41d3da18be350
“Enter <talktoparthi@gmail.com>
2020-08-08 18:34:43 +0530

    commited

--- userregistration.sh
+++ userregistration.sh
@@ -1,54 +1,51 @@
 #!/bin/bash -x
 
-echo "Enter your first name and last name :" 
+#echo "Enter your first name and last name :" 
 
-read firstName 
-read lastname
+#read firstName 
+#read lastname
 
-pattern="^[A-Z]{1}[a-z]{2}$" 
+#pattern="^[A-Z]{1}[a-z]{2}$" 
 
-if [[ $firstName =~ $pattern ]] 
+#if [[ $firstName =~ $pattern ]] 
 
-then 
-	echo "Name is valid"
+#then 
+	#echo "Name is valid"
 
-else 
-	echo "Name is invalid" 
+#else 
+	#echo "Name is invalid" 
 
 
-fi
+#fi
 
 
 #echo "Enter your last name :" 
 
 
-pattern="^[A-Z]{1}[a-z]{2}$" 
+#pattern="^[A-Z]{1}[a-z]{2}$" 
 
-if [[ $lastname =~ $pattern ]] 
+#if [[ $lastname =~ $pattern ]] 
 
-then 
-	echo "Name is valid"
+#then 
+	#echo "Name is valid"
 
-else 
-	echo "Name is invalid" 
+#else 
+	#echo "Name is invalid" 
 
+#fi
+
+echo " Enter your email ID : "
+read email
+
+
+#pattern="^[0-9]*$"
+#pattern="^[1-9]{1}[0-9]{5}$"
+
+pattern="^([a-z0-9]+)@([a-z]+)\.([a-z]{2,5})$"
+
+if [[ $email =~ $pattern ]]
+then
+        echo true;
+else
+        echo false;
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
