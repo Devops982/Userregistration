50fb05d8351e4777a7c9c7dec5423cf9fc891288
“Enter <talktoparthi@gmail.com>
2020-08-08 16:57:09 +0530

    new changes

--- userregistration.sh
+++ userregistration.sh
@@ -1,2 +1,35 @@
 #!/bin/bash -x
 
+echo "Enter your first name :" 
+
+read firstName 
+
+pattern="^[A-Z]{1}[a-z]{2}$" 
+
+if [[ $firstName =~ $pattern ]] 
+
+then 
+	echo "Name is valid"
+
+else 
+	echo "Name is invalid" 
+
+
+fi
+
+
+echo "Enter your last name :" 
+
+read lastname
+
+pattern="^[A-Z]{1}[a-z]{2}$" 
+
+if [[ $lastname =~ $pattern ]] 
+
+then 
+	echo "Name is valid"
+
+else 
+	echo "Name is invalid" 
+
+fi
